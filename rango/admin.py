321049f2e240120a103a7308c12226b98170e35f
Kamil Nowakowski <2911304n@student.gla.ac.uk>
2025-01-26 18:13:50 +0000

    Applied minor fix to the population script, and then recreated and repopulated database

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')   # display title, category, and url fields in admin interface
+
 # Register your models here.
 admin.site.register(Category)              # register Category model
-admin.site.register(Page)                  # register Page model
-
+admin.site.register(Page, PageAdmin)                  # register Page model
