52d70a84f20ee06b7ae7b2032ac9d4448303a09d
Kamil Nowakowski <2911304n@student.gla.ac.uk>
2025-01-26 15:42:19 +0000

    Created and migrated the database. Configured the admin interface

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)                 # max length of 128 characters for name field in Category model  
+
+    class Meta:                                                          # Meta class for Category model
+        verbose_name_plural = 'Categories'                               # verbose name for Category model
+
+    def __str__(self):                                                   # string representation of Category model
+        return self.name                                                 # return name of Category model
+    
+
+# Page model
+class Page(models.Model):   
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)     # foreign key to Category model with CASCADE delete
+    title = models.CharField(max_length=128)                             # max length of 128 characters for title field in Page model 
+    url = models.URLField()                                              # URL field for url field in Page model 
+    views = models.IntegerField(default=0)                               # default value is 0
+
+    def __str__(self):                                                   # string representation of Page model
+        return self.title                                                # return title of Page model
